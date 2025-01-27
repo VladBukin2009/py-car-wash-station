a5a891f34be4a3e2a6322fc528bb9a93b26dbfa5
Vlad Bukin <vbukinnn2009@gmail.com>
2025-01-27 11:30:05 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+        self, comfort_class: int, clean_mark: int, brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
+
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: int, average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -23,20 +29,23 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(
-            car.comfort_class * (self.clean_power - car.clean_mark) *
-            self.average_rating / self.distance_from_city_center, 1
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
         )
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def rate_service(self, new_rating: float) -> None:
-        total_rating = self.average_rating * self.count_of_ratings
-        total_rating += new_rating
+        total_rating = (
+            self.average_rating * self.count_of_ratings + new_rating
+        )
         self.count_of_ratings += 1
         self.average_rating = round(
-            total_rating / self.count_of_ratings,
-            1
+            total_rating / self.count_of_ratings, 1
         )
